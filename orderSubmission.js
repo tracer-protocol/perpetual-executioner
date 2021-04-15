d4d63f55ddbff18c9a9eb7078311f553db07126d
raymogg <raymondmogg@gmail.com>
2021-04-15 17:51:17 +1000

    integrated utils package. Updated tests

--- orderSubmission.js
+++ orderSubmission.js
@@ -1,12 +1,13 @@
-let serialiseOrder = require("./orderSerialisation").serialiseOrder
+//let serialiseOrder = require("./orderSerialisation").serialiseOrder
+let omeOrderToOrder = require("@tracer-protocol/tracer-utils").omeOrderToOrder
 const web3 = require("web3")
 
 /**
  * Submit orders to the on chain trader contract.
  */
 const submitOrders = async (makerOrders, takerOrders, contract, market, sendingAccount) => {
-    let serialisedMakeOrders = makerOrders.map((order) => serialiseOrder(order))
-    let serialisedTakeOrders = takerOrders.map((order) => serialiseOrder(order))
+    let serialisedMakeOrders = makerOrders.map((order) => omeOrderToOrder(web3, order))
+    let serialisedTakeOrders = takerOrders.map((order) => omeOrderToOrder(web3, order))
     try {
         let gas = await contract.methods.executeTrade(serialisedMakeOrders, serialisedTakeOrders, web3.utils.toChecksumAddress(market)).estimateGas({ from: sendingAccount })
         let txn = await contract.methods.executeTrade(serialisedMakeOrders, serialisedTakeOrders, web3.utils.toChecksumAddress(market)).send({ from: sendingAccount, gas: gas })
