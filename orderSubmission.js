adfdf0c2897f90d19b9fbfac526f5d591fb35a7c
raymogg <raymondmogg@gmail.com>
2021-06-09 17:18:15 +1000

    make gas limit configurable

--- orderSubmission.js
+++ orderSubmission.js
@@ -5,12 +5,11 @@ const web3 = require("web3")
 /**
  * Submit orders to the on chain trader contract.
  */
-const submitOrders = async (makerOrders, takerOrders, contract) => {
+const submitOrders = async (makerOrders, takerOrders, contract, gasLimit) => {
     let serialisedMakeOrders = makerOrders.map((order) => omeOrderToOrder(web3, order))
     let serialisedTakeOrders = takerOrders.map((order) => omeOrderToOrder(web3, order))
     try {
-        let txn = await contract.executeTrade(serialisedMakeOrders, serialisedTakeOrders)
-        return txn
+        return await contract.executeTrade(serialisedMakeOrders, serialisedTakeOrders, { gasLimit: gasLimit})
     } catch (e) {
         console.error(e)
         throw e
