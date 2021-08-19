ed07b53c736c2e6272c26e876c8050a8f8a2886b
Chris Martin <chris@crunchycloud.io>
2021-08-19 15:59:57 +1000

    [deployment] add additional output in setupRelease script

--- deployment/setupRelease.sh
+++ deployment/setupRelease.sh
@@ -5,7 +5,7 @@ yarn version
 version=$(cat package.json | jq -r '.version')
 docker_image="gcr.io/tracer-protocol-testing/executioner"
 
-read -p "Create new docker image with tag $docker_image:$version? (y/n): " confirm_version
+read -p "create new docker image with tag $docker_image:$version? (y/n): " confirm_version
 
 if [ $confirm_version != "y" ]
 then
@@ -19,6 +19,8 @@ docker build . -t $docker_image:$version
 echo "running docker push $docker_image:$version"
 docker push $docker_image:$version
 
+echo "built and pushed $docker_image:$version"
+
 read -p "Update k8s deployment config with new image tag? (y/n): " update_kubernetes
 
 if [ $update_kubernetes != "y" ]
@@ -29,3 +31,5 @@ fi
 
 # update the deploy.yaml file
 sed -i -e "s|$docker_image:[0-9].[0-9].[0-9]|$docker_image:$version|g" deployment/deploy.yaml
+
+echo "updated k8s deployment config with new image"
